e5b13518ccb17edfed0b12b71bf74a1ef57529b5
Invisigoth <invisigoth@saturnV.homenet.telecomitalia.it>
2024-07-11 23:33:49 +0200

    Rearrange Tavern: select 'emacsclient' as default API send method

--- lib/tavern.ex
+++ lib/tavern.ex
@@ -6,17 +6,19 @@ defmodule Tavern do
   """
   defdelegate subscribe(), to: Tavern.Api
 
-  def emacs_eval(source) when is_binary(source), do: emacs_eval(:el, source)
-  def emacs_eval(source), do: emacs_eval(:ex, source)
-  def emacs_eval(:el, source) when is_binary(source) do
+  def emacs_eval(source), do: emacs_eval(source, :emacsclient)
+  def emacs_eval(source, mode) when is_binary(source) do
     source
-    |> evaluate()
+    |> evaluate(mode)
   end
 
-  def emacs_eval(:ex, source) do
-    emacs_eval(:el, source |> to_elisp!())
+  def emacs_eval(source, mode) do
+    source
+    |> to_elisp!
+    |> emacs_eval(mode)
   end
 
   defp to_elisp!(source), do: Elisp.sexp(source)
   defp evaluate(source), do: Api.send_message(source)
+  defp evaluate(source, mode), do: Api.send_message(source, mode)
 end
