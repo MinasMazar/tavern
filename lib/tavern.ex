74de74673db67d5cf6661c034cd4b7f9daf0581c
MinasMazar <5578468+MinasMazar@users.noreply.github.com>
2024-10-11 18:04:12 +0200

    Fixes

--- lib/tavern.ex
+++ lib/tavern.ex
@@ -1,18 +1,26 @@
 defmodule Tavern do
+  @default_timeout 300
   alias Tavern.{Elisp, Api.EmacsClient}
 
   @moduledoc """
   Documentation for `Tavern`.
   """
-
-  def emacs_eval(source) when is_binary(source) do
-    source
-    |> Api.EmacsClient.emacs_eval()
+  def emacs_eval(source) do
+    emacs_eval(source, @default_timeout)
   end
 
-  def emacs_eval(source) do
+  def emacs_eval(source, timeout) when is_binary(source) do
+    source
+    |> EmacsClient.emacs_eval(timeout)
+  end
+
+  def emacs_eval(source, timeout) do
     source
     |> Elisp.sexp()
-    |> EmacsClient.emacs_eval()
+    |> EmacsClient.emacs_eval(timeout)
+  end
+
+  def greet! do
+    emacs_eval([:message, "hello from Elixir!"])
   end
 end
